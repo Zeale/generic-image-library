87a9e65ee4138319cba7222f6e0b58732eec31a6
Zeale <zeale@dusttoash.org>
2018-11-14 00:35:26 -0600

    Updated Image class

---
+++ src/org/alixia/gil/Image.java
@@ -0,0 +1,22 @@
+package org.alixia.gil;
+
+public class Image {
+
+	private Color[][] image;
+
+	public Image(int length, int width) {
+		image = new Color[length][width];
+	}
+
+	public void resize(int length, int height) {
+		// TODO Method stub
+	}
+
+	public void resizeLength(int lenght) {
+		// TODO Method stub
+	}
+
+	public void resizeHeight(int height) {
+		// TODO Method stub
+	}
+}
